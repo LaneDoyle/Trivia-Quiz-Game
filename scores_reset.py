0d7f9fcac757f43abb147261bbcb095957e437a6
LaneDoyle <ldoyle@stcharlessd.org>
2020-03-10 10:56:26 -0500

    new file:   questions_lib.pickle 	modified:   scores_reset.py

--- scores_reset.py
+++ scores_reset.py
@@ -11,7 +11,8 @@ questions = {1: ["When was the war of 1812?", "1812", "1776", "1790", "1810"],
                  "The Atlantic Ocean", "El Caribe"],
              3: ["Who is the lead singer of Pearl Jam?", "Eddie Vedder", "Buddie Hollie", "Van Houten", 
                  "James P. Sullivan"],
-             4: ["Im cool", "Yes",  "Yes",  "Yes",  "Yes"]}
+             4: ["What is the name of the woman Mario is trying to save in the original Donkey Kong?", 
+                 "Pauline", "Princess Peach", "Princess Daisy", "Rosalina"]}
 
 data_file = open("questions_lib.pickle", "wb")
 pickle.dump(questions, data_file)
